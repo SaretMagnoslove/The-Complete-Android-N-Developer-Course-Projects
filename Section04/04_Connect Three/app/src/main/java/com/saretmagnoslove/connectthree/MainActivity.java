7963c74c57cb0dd14b18e925dc5ff3e3e9387337
SaretMagnoslove <magnoslove@gmail.com>
2018-01-06 01:54:03 +0200

    Add full game without apk

--- Section04/04_Connect Three/app/src/main/java/com/saretmagnoslove/connectthree/MainActivity.java
+++ Section04/04_Connect Three/app/src/main/java/com/saretmagnoslove/connectthree/MainActivity.java
@@ -1,13 +1,20 @@
 package com.saretmagnoslove.connectthree;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
+
+    boolean gameIsActive = true;
+
     int[] gameState = {2,2,2,2,2,2,2,2,2};
     int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
@@ -17,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tappedCounter] == 2) {
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
 
             gameState[tappedCounter] = activePlayer;
 
@@ -42,12 +49,77 @@ public class MainActivity extends AppCompatActivity {
                         gameState[winingPosition[1]] == gameState[winingPosition[2]]
                         && gameState[winingPosition[0]] != 2) {
 
-                    System.out.println(gameState[winingPosition[0]]);
+                    gameIsActive = false;
+
+                    String winner = "Red";
+
+                    if (gameState[winingPosition[0]] == 0) {
+
+                        winner = "Yellow";
+                    }
+
+                    TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+                    winnerMessage.setText(winner + " has Won!");
+
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.layer);
+                    layout.setVisibility(view.VISIBLE);
+
+                } else {
+
+                    boolean gameIsOver = true;
+
+                    for (int counterState : gameState) {
+
+                        if (counterState == 2) gameIsOver = false;
+
+                    }
+
+                    if (gameIsOver) {
+
+                        TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+
+                        winnerMessage.setText("It's a draw");
+
+                        LinearLayout layout = (LinearLayout)findViewById(R.id.layer);
+
+                        layout.setVisibility(View.VISIBLE);
+
+                    }
+
+
                 }
             }
 
         }
+
     }
+
+    public void playAgain(View view) {
+
+        gameIsActive = true;
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.layer);
+
+        layout.setVisibility(View.INVISIBLE);
+
+        activePlayer = 0;
+
+        for (int i = 0; i < gameState.length; i++) {
+
+            gameState[i] = 2;
+
+        }
+
+        GridLayout  gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+
+        for (int i = 0; i< gridLayout.getChildCount(); i++) {
+
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
