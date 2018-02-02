8cc86ca5e90a1fe71cc2940eb277cd94f2631d46
SaretMagnoslove <magnoslove@gmail.com>
2018-02-02 21:33:32 +0200

    ADD finding device shape

---
+++ Section12/Lecture 186 - Adapting Apps For Different Watch Faces (Android Studio files)/AndroidWearDemo/app/src/main/java/com/robpercival/androidweardemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.robpercival.androidweardemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+
+                if (findViewById(R.id.rect_layout) != null) {
+
+                    Log.i("Info", "Rectangular layout");
+
+                } else {
+
+                    Log.i("Info", "Round Layout");
+
+                }
+
+            }
+        });
+    }
+}
---
+++ Section12/Lecture 186 - Adapting Apps For Different Watch Faces (Android Studio files)/SquareOrRound/app/src/main/java/com/robpercival/squareorround/MainActivity.java
@@ -0,0 +1,39 @@
+package com.robpercival.squareorround;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    public void showWatchShape(View view) {
+
+        String message = "Rectangular";
+
+        if (findViewById(R.id.round_layout) != null) {
+
+            message = "Round";
+
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+
+
+            }
+        });
+    }
+}
